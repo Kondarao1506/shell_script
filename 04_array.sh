9b6ce9652fef2db868d957a17c6d9eb21e9f5b36
Kondarao1506 <Kondarao1506@gmail.com>
2025-04-15 08:43:52 +0530

    variable types and array

--- 04_array.sh
+++ 04_array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Fruits=("apple","pappaya","orange") #index number starts from 0
+Fruits=("apple" "pappaya" "orange") #index number starts from 0
 echo "the first fruit name is ${Fruits[0]}"
 echo "the second fruit name is ${Fruits[1]}"
 echo "the third fruit name is ${Fruits[2]}"
