270e128abc6eb30bee06f4e259d83e01a5d20c0e
Kondarao1506 <Kondarao1506@gmail.com>
2025-04-16 13:54:44 +0530

    did modifications on expense project

--- shell_expense/mysql.sh
+++ shell_expense/mysql.sh
@@ -4,7 +4,7 @@ G="\e[32m"
 N="\e[0m"
 FOLDER_PATH="/var/log/expense_logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(date +%Y-%M-%D-%H-%M-%S)
+TIME_STAMP=$(date +%Y-%M)
 LOG_FILE="$FOLDER_PATH/$SCRIPT_NAME-$TIME_STAMP.log"
 USERID=$(id -u)
 ROOT(){
