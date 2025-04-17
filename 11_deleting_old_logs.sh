eb7b40941e3fcf4e3b27496a5cbd51c46d6c4ac2
Kondarao1506 <Kondarao1506@gmail.com>
2025-04-17 17:28:33 +0530

    crontab uses

--- 11_deleting_old_logs.sh
+++ 11_deleting_old_logs.sh
@@ -2,7 +2,7 @@
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-FILE_DIRECTORY="/var/log/logfiles"
+FILE_DIRECTORY=" /var/log/applogs"
 if [ -d $FILE_DIRECTORY ] # HERE -D is used to check the directory exists or not
 then
     echo -e "$FILE_DIRECTORY $G Exists...$N"
@@ -11,10 +11,10 @@ else
     exit 1
 fi
 FILE=$(find $FILE_DIRECTORY -name "*.log" -mtime +14)
-while IFS -r line
+while IFS -r lin
 do
-echo -e "$line $G deleting...$N"
-rm -rf $line
+echo -e "$lin $G deleting...$N"
+rm -rf $lin
 done <<< $FILE
 
 #to create file with particular day touch -d 20240801 java.log
