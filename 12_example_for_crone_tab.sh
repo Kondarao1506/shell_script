19e37a77486d86d2e445d8e698dfd284548d6e07
Kondarao1506 <Kondarao1506@gmail.com>
2025-04-17 17:18:06 +0530

    crontab uses

--- 12_example_for_crone_tab.sh
+++ 12_example_for_crone_tab.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-LOG_DIR="/var/log"
+LOG_DIR="/var/log/applogs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_DIR/$SCRIPT_NAME-$TIME.log"
@@ -8,7 +8,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "PLEASE RUN WITH ROOT PRIVILAGES sudo sh $0.sh"
+    echo "PLEASE RUN WITH ROOT PRIVILAGES sudo sh $0.sh" | tee -a $LOG_FILE
     exit 1
 fi
 for i in {1..10}
